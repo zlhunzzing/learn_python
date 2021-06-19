ba6f808649312d8f55f00e6028b6018761e4870a
Jihun Kim <zlhunzzing@gmail.com>
2021-06-19 20:11:22 +0900

    변수, , str()

--- practice.py
+++ practice.py
@@ -10,3 +10,14 @@ print("ㅋ" * 10)
 print(True)
 print(False)
 print(not True)
+
+# 변수
+animal = "뽀삐"
+name = "뽀미"
+age = 1
+is_adult = age == 3
+
+print("우리집 강아지의 이름은 " + name + "에요")
+name = animal
+print(name, "의 나이는 " + str(age), "살이에요")
+print("뽀미는 3살인가요?" + str(is_adult))
