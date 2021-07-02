8cbb01740a2414c256942de56da8775430f30f35
Jihun Kim <zlhunzzing@gmail.com>
2021-07-02 10:20:29 +0900

    funcion(def), parameter

---
+++ function.py
@@ -0,0 +1,14 @@
+## 함수
+def open_account():
+    print("새로운 계좌가 생성되었습니다.")
+
+open_account()
+
+## 파라미터, default parameter
+
+## 파라미터 키워드 
+
+def profile(name, age, main_lang):
+    print(name, age, main_lang)
+
+profile(name="뽀미", main_lang="한글", age=1)
