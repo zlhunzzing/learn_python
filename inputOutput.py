22edf7c29ff1c7ea1f28d8cd0ecfdd3546334efe
Jihun Kim <zlhunzzing@gmail.com>
2021-07-07 08:45:32 +0900

    파일입출력

--- inputOutput.py
+++ inputOutput.py
@@ -14,15 +14,49 @@
 ## 다양한 출력포맷
 
 # 빈자리 우측정렬
-print("{0: >10}".format(500))
-# # 양수 음수
-print("{0: >+10}".format(500))
-# # 왼측정렬
-print("{0:_<-10}".format(-500))
-# # 3자리 콤마
-print("{0:,}".format(10000000))
-# # to much smile
-print("{0:^<+30,}".format(10000000))
-# # 소수점
-print("{0:f}".format(5/3))
-print("{0:.2f}".format(5/3))
+# print("{0: >10}".format(500))
+# # # 양수 음수
+# print("{0: >+10}".format(500))
+# # # 왼측정렬
+# print("{0:_<-10}".format(-500))
+# # # 3자리 콤마
+# print("{0:,}".format(10000000))
+# # # to much smile
+# print("{0:^<+30,}".format(10000000))
+# # # 소수점
+# print("{0:f}".format(5/3))
+# print("{0:.2f}".format(5/3))
+
+## 파일입출력
+
+# score_file = open("score.txt", "w", encoding="utf8")
+# print("수학 : 0", file=score_file)
+# print("영어 : 50", file=score_file)
+# score_file.close()
+
+# 이어서 쓰기
+# score_file = open("score.txt", "a", encoding="utf8")
+# score_file.write("과학 : 80")
+# score_file.write("\n코딩 : 100")
+# score_file.close()
+
+# 읽기
+# score_file = open("score.txt", "r", encoding="utf8")
+# print(score_file.read())
+# print(score_file.readline(), end="")
+# print(score_file.readline())
+# print(score_file.readline())
+# print(score_file.readline())
+# score_file.close()
+
+# 여러 줄출력
+score_file = open("score.txt", "r", encoding="utf8")
+while True:
+    line = score_file.readline()
+    if not line:
+        break
+    print(line)
+# score_file.close()
+lines = score_file.readlines() # list
+for line in lines:
+    print(line, end="")
