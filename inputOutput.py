991284a833a6a6294937ac341cdd7c8ab92debe7
Jihun Kim <zlhunzzing@gmail.com>
2021-07-09 07:38:32 +0900

    with

--- inputOutput.py
+++ inputOutput.py
@@ -62,14 +62,24 @@
 #     print(line, end="")
 
 ## pickle
-import pickle
+# import pickle
 # profile_file = open("profile.pickle", "wb")
 # profile = {"이름":"김지훈", "나이":17, "직업":"여고생"}
 # print(profile)
 # pickle.dump(profile, profile_file)
 # profile_file.close()
 
-profile_file = open("profile.pickle", "rb")
-profile = pickle.load(profile_file)
-print(profile)
-profile_file.close()
+# profile_file = open("profile.pickle", "rb")
+# profile = pickle.load(profile_file)
+# print(profile)
+# profile_file.close()
+
+## with
+# with open("profile.pickle", "rb") as profile_file:
+#     print(pickle.load(profile_file))
+
+# with open("study.txt", "w", encoding="utf8") as study_file:
+#     study_file.write("파이썬을 열심히 공부하고 있어요")
+
+with open("study.txt", "r", encoding="utf8") as study_file:
+    print(study_file.read())
