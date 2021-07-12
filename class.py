03621ce4214bbffa13b4b89757255452b2ed1806
Jihun Kim <zlhunzzing@gmail.com>
2021-07-12 20:11:22 +0900

    메소드

--- class.py
+++ class.py
@@ -5,24 +5,56 @@
 
 # print("{} 유닛이 생성되었습니다.".format(name))
 
+# class Unit:
+#     def __init__(self, name, hp, damage):
+#         self.name = name
+#         self.hp = hp
+#         self.damage = damage
+#         print("{0} 유닛이 생성 되었습니다.".format(self.name))
+
+# marine1 = Unit("마린", 40, 5)
+# marine2 = Unit("마린", 40, 5)
+
+## 클래스 확장
+
+# 레이스 : 공중 유닛
+# wraith1 = Unit("레이스", 80, 5)
+
+# # # 마인드 컨트롤 : 상대방 유닛을 내 것으로
+# wraith2 = Unit("빼앗은 레이스", 80, 5)
+# wraith2.clocking = True
+
+# if wraith2.clocking == True:
+#     print("{0} 는 현재 클로킹 상태입니다.".format(wraith2.name))
+
+## 메소드
+
 class Unit:
     def __init__(self, name, hp, damage):
         self.name = name
         self.hp = hp
         self.damage = damage
         print("{0} 유닛이 생성 되었습니다.".format(self.name))
+        print("체력 {0}, 공격력 {1}".format(self.hp, self.damage))
 
-marine1 = Unit("마린", 40, 5)
-marine2 = Unit("마린", 40, 5)
+class AttackUnit(Unit):
+    def __init__(self, name, hp, damage):
+        self.name = name
+        self.hp = hp
+        self.damage = damage
 
-## 클래스 확장
+    def attack(self, location):
+        print("{0} : {1} 방향으로 적군을 공격합니다. [공격력 {2}"\
+            .format(self.name, location, self.damage))
 
-# 레이스 : 공중 유닛
-wraith1 = Unit("레이스", 80, 5)
-
-# # 마인드 컨트롤 : 상대방 유닛을 내 것으로
-wraith2 = Unit("빼앗은 레이스", 80, 5)
-wraith2.clocking = True
-
-if wraith2.clocking == True:
-    print("{0} 는 현재 클로킹 상태입니다.".format(wraith2.name))
+    def damaged(self, damage):
+        print("{0} : {1} 데미지를 입었습니다.".format(self.name, damage))
+        self.hp -= damage
+        print("{0} : 현재 체력은 {1} 입니다.".format(self.name, self.hp))
+        if self.hp <= 0:
+            print("{0} : 파괴되었습니다.".format(self.name))
+# 파뱃
+firebat1 = AttackUnit("파이어뱃", 50, 16)
+firebat1.attack("5시")
+firebat1.damaged(25)
+firebat1.damaged(25)
