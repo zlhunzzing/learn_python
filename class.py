59e784645c820a948a74415bd96d3e5db0aaaea0
Jihun Kim <zlhunzzing@gmail.com>
2021-07-14 06:50:19 +0900

    다중상속

--- class.py
+++ class.py
@@ -55,7 +55,34 @@ class AttackUnit(Unit):
         if self.hp <= 0:
             print("{0} : 파괴되었습니다.".format(self.name))
 # 파뱃
-firebat1 = AttackUnit("파이어뱃", 50, 16)
-firebat1.attack("5시")
-firebat1.damaged(25)
-firebat1.damaged(25)
+# firebat1 = AttackUnit("파이어뱃", 50, 16)
+# firebat1.attack("5시")
+# firebat1.damaged(25)
+# firebat1.damaged(25)
+
+## 상속
+
+## 다중 상속
+
+# 드랍쉽 : 공중 유닛, 공격 x
+
+# 날 수 있는 기능을 가진 클래스
+
+class Flyable:
+    def __init__(self, flying_speed):
+        self.flying_speed = flying_speed
+
+    def fly(self, name, location):
+        print("{0} : {1} 방향으로 날아갑니다. [속도 {2}]"\
+            .format(name, location, self.flying_speed))
+
+# 공중 공격 유닛 클래스
+class FlyableAttackUnit(AttackUnit, Flyable):
+    def __init__(self, name, hp, damage, flying_speed):
+        AttackUnit.__init__(self, name, hp, damage) # 지상 스피드 0
+        Flyable.__init__(self, flying_speed)
+
+# 발키리 : 공중 공격 유닛
+
+valkyrie = FlyableAttackUnit("발키리", 200, 6, 5)
+valkyrie.fly(valkyrie.name, "3시")
