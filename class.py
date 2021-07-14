ee93d69aedef49eab023cf81a30c762057510ef5
Jihun Kim <zlhunzzing@gmail.com>
2021-07-15 08:39:47 +0900

    메소드 오버라이딩

--- class.py
+++ class.py
@@ -30,16 +30,22 @@
 ## 메소드
 
 class Unit:
-    def __init__(self, name, hp):
+    def __init__(self, name, hp, speed):
         self.name = name
         self.hp = hp
+        self.speed = speed
+
+    def move(self, location):
+        print("[지상 유닛 이동]")
+        print("{0} : {1} 방향으로 이동합니다. [속도 {2}]"\
+            .format(self.name, location, self.speed))
         # self.damage = damage
         # print("{0} 유닛이 생성 되었습니다.".format(self.name))
         # print("체력 {0}, 공격력 {1}".format(self.hp, self.damage))
 
 class AttackUnit(Unit):
-    def __init__(self, name, hp, damage):
-        Unit.__init__(self, name, hp)
+    def __init__(self, name, hp, speed, damage):
+        Unit.__init__(self, name, hp, speed)
         # self.name = name
         # self.hp = hp
         self.damage = damage
@@ -79,10 +85,25 @@ class Flyable:
 # 공중 공격 유닛 클래스
 class FlyableAttackUnit(AttackUnit, Flyable):
     def __init__(self, name, hp, damage, flying_speed):
-        AttackUnit.__init__(self, name, hp, damage) # 지상 스피드 0
+        AttackUnit.__init__(self, name, hp, 0, damage) # 지상 스피드 0
         Flyable.__init__(self, flying_speed)
+
+    def move(self, location):
+        print("[공중 유닛 이동]")
+        self.fly(self.name, location)
 
 # 발키리 : 공중 공격 유닛
 
-valkyrie = FlyableAttackUnit("발키리", 200, 6, 5)
-valkyrie.fly(valkyrie.name, "3시")
+# valkyrie = FlyableAttackUnit("발키리", 200, 6, 5)
+# valkyrie.fly(valkyrie.name, "3시")
+
+## 메소드 오버라이딩
+
+# 벌쳐 : 지상 유닛
+vulture = AttackUnit("벌쳐", 80, 10, 20)
+
+battlecruiser = FlyableAttackUnit("배틀크루저", 500, 25, 3)
+
+vulture.move("11시")
+# battlecruiser.fly(battlecruiser.name, "9시")
+battlecruiser.move("9시")
