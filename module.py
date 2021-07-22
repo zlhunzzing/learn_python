e016c705c90839f445232c18212c0ac97b434cd3
Jihun Kim <zlhunzzing@gmail.com>
2021-07-22 20:11:22 +0900

    module

---
+++ module.py
@@ -0,0 +1,7 @@
+## 모듈
+
+import setModule
+
+setModule.price(4)
+
+# as 별명, import * - 다 쓰기, import 함수 1, 함수 2 (as 별명)
---
+++ setModule.py
@@ -0,0 +1,2 @@
+def price(people):
+    print("가격은 {0}원입니다".format(people * 1000))
