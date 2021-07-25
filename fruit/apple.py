cf3d54ab8f6be63335dc96ff5c5efd3e9cd367dd
Jihun Kim <zlhunzzing@gmail.com>
2021-07-25 20:11:22 +0900

    __main__, 모듈 직접 실행

--- fruit/apple.py
+++ fruit/apple.py
@@ -1,3 +1,10 @@
 class ApplePackage:
     def detail(self):
         print("애플")
+
+if __name__ == "__main__":
+    print("Apple 모듈을 직접 실행")
+    excute = ApplePackage()
+    excute()
+else:
+    print("Aplle 모듈을 외부에서 오출")
