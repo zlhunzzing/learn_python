06982edba2639bb1359f8450af264ad0cb4730ba
Jihun Kim <zlhunzzing@gmail.com>
2021-07-28 20:11:22 +0900

    내장함수:

---
+++ builtInFunc.py
@@ -0,0 +1,11 @@
+## input : 사용자 입력을 받는 함수
+# languaage = input("무슨 언어를 좋아하세요?")
+# print("{0}은 아주 좋은 언어입니다!".format(language))
+
+# dir : 어떤 객체를 넘겨줬을 때 그 객체가 어떤 변수와 함수를 가지고 있는지 표시
+print(dir())
+import random # 외장 함수
+print(dir())
+
+lst = [1] # 배열 혹은 문자열 내장 함수 확인 가능
+print(dir(lst))
